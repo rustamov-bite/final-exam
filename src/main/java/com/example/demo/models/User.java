fdb4f8621c825a257f8c4964ea28d8385c9d6c93
rustamov-bite <assasins_murza006@mail.ru>
2021-06-26 09:40:20 +0600

    added ID for User

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -5,8 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -15,4 +14,10 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
 }
